4cc06877fd71135b83104179db9eb2ed952e48fa
yuwyuw <915998347@qq.com>
2024-05-22 22:35:52 +0800

    fist add

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    files:["**/*.js", "**/*.tsx", "**/*.jsx"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  }
+];
---
+++ pages/component-h5/test1/index.tsx
@@ -0,0 +1,10 @@
+import React,{ FC }  from "react";
+
+interface InfoProps {
+
+}
+
+const Test: FC<InfoProps> = (props) => {
+  return (<div>hello world</div>)
+} 
+export default Test;
