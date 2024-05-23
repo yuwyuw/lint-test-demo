fbb4ab6e66948f664921b63e4436f530942d1e2d
yuwyuw <915998347@qq.com>
2024-05-23 16:55:12 +0800

    ci(commitlint): added button as a type of commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,8 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+      "@commitlint/config-conventional"
+  ],
   rules: {
-    'scope-case': [0],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-        'ui' //针对css方面的改变新增的类型
-      ]
-    ]
+      "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "button"]],
   }
 }
