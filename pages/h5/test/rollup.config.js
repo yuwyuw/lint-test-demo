4b764ed9a569d7330645179cb52b2b08030a0af7
yuwyuw <915998347@qq.com>
2024-05-24 22:18:21 +0800

    fix: add

---
+++ pages/h5/test/rollup.config.js
@@ -0,0 +1,35 @@
+// rollup.config.mjs
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import packageJson from './package.json';
+export default {
+	input: 'index.jsx',
+	output: {
+		name: 'index',
+    dir: 'dist',
+    format: 'commonjs'
+	},
+	plugins: [
+		external(),
+		commonjs(),
+		postcss(),
+		terser(),
+		resolve(),
+		babel({ 
+			exclude: 'node_modules/**',
+			presets: ['@babel/env', '@babel/preset-react']
+		})
+	],
+	external: [
+		'react',
+		'prop-types',
+	],
+	globals: {
+		react: "React"
+	}
+};
